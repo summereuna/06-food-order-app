be7da89c5ab2967c1c1f80495109e389dc8de949
summereuna <summereuna@gmail.com>
2023-05-01 17:19:26 +0900

    Add header cart button

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
-import classes from "./Header.module.css";
+import HeaderCardButton from "../UI/HeaderCartButton";
 //로컬 이미지 임포트
 import mealImg from "../../assets/meals.jpeg";
+import classes from "./Header.module.css";
 
 const Header = () => {
   return (
     <>
       <header className={classes.header}>
         <h1>Ordinary Meals</h1>
-        <button>Cart</button>
+        <HeaderCardButton></HeaderCardButton>
       </header>
       <div className={classes["main-image"]}>
         <img src={mealImg} alt="A table full of delicious food!" />
---
+++ src/components/UI/HeaderCartButton.js
@@ -0,0 +1,25 @@
+import classes from "./HeaderCartButton.module.css";
+
+// icon은 이아웃에서 사용하니까 레이아웃 폴더에 저장하거나 혹은 장바구니 전용 아이콘이니 장바구니 기능(feature) 폴더인 cart 폴더에 저장해도 됌
+import CartIcon from "../Cart/CartIcon";
+
+const HeaderCartButton = (props) => {
+  return (
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>3</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
+
+/*
+버튼
+ ㄴ카트 아이콘
+ ㄴ텍스트
+ ㄴ작은 배지(항목 수)
+*/
